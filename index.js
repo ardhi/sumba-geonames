b9dae0d2171826ff05e23b18b4bc7826d9d55197
Ardhi Lukianto <ardhi@lukianto.com>
2025-08-23 23:43:07 +0700

    Updates

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 async function factory (pkgName) {
   const me = this
 
-  return class SumbaGeonames extends this.lib.BajoPlugin {
+  return class SumbaGeonames extends this.lib.Plugin {
     constructor () {
       super(pkgName, me.app)
       this.alias = 'geonames'
