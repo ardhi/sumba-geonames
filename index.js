e7f38d7049c39928a32cc5b3379aed428d910cd5
Ardhi Lukianto <ardhi@lukianto.com>
2025-09-28 21:38:45 +0700

    Updates

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+/**
+ * Plugin factory
+ *
+ * @param {string} pkgName - NPM package name
+ * @returns {class}
+ */
 async function factory (pkgName) {
   const me = this
 
+  /**
+   * SumbaGeonames class
+   *
+   * @class
+   */
   class SumbaGeonames extends this.app.pluginClass.base {
     static alias = 'geonames'
     static dependencies = ['dobo']
